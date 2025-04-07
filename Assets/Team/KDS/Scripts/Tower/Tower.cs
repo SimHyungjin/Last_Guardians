a995617168e97b531c2e966d70942282a87b195e
dongseok-kim <kdsehdtjr12@gmail.com>
2025-04-07 14:53:56 +0900

    Feat:TowerSO,testTower
    타워 SO와 테스트타워생성

---
+++ Assets/Team/KDS/Scripts/Tower/Tower.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public TowerData towerdata;
+    public GameObject towerGhost;
+
+    public void MoveTower()
+    {
+
+    }
+}

019a78771b5813d029c74c4fa35829249df84edf
dongseok-kim <kdsehdtjr12@gmail.com>
2025-04-07 11:11:03 +0900

    Feat:Newtonsoft_Json

--- Assets/Team/KDS/Manager/DataDownLoadManager.cs
+++ Assets/Team/KDS/Manager/DataDownLoadManager.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System;
+
+
+[CustomEditor(typeof(DataDownLoadManager))]
+public class SheetDownButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        DataDownLoadManager fnc = (DataDownLoadManager)target;
+        if (GUILayout.Button("Download SheetData"))
+        {
+            fnc.StartDownload(true);
+        }
+    }
+}
+
 
 public class DataDownLoadManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<DataSO> abilityDataSO = new List<DataSO>();
+    const string URL_DataSheet= "https://docs.google.com/spreadsheets/d/11uh3qkFXuMsowtu7qrmO66nYMx46C2P9UHh3c1eHVu4/export?format=tsv&range=A2:F6";
+
+    public void StartDownload(bool renameFiles)
     {
-        
+        StartCoroutine(DownloadData(renameFiles));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DownloadData(bool renameFiles)
     {
-        
+        UnityWebRequest www = UnityWebRequest.Get(URL_DataSheet);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            string tsvText = www.downloadHandler.text;
+            string json = ConvertTSVToJson(tsvText);
+
+            JArray jsonData = JArray.Parse(json); // JSON 문자열을 JArray로 변환
+            ApplyDataToSO(jsonData, renameFiles);
+        }
+        else
+        {
+            Debug.LogError("데이터 가져오기 실패: " + www.error);
+        }
+
+        // CreatePrefabs();
+        ApplyDataSO();
+    }
+
+    string ConvertTSVToJson(string tsv)
+    {
+        string[] lines = tsv.Split('\n'); // 줄 단위로 분리
+        if (lines.Length < 2) return "[]"; // 데이터가 없으면 빈 JSON 배열 반환
+
+        string[] headers = lines[0].Split('\t'); // 첫 번째 줄을 헤더로 사용
+        JArray jsonArray = new JArray();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] values = lines[i].Split('\t'); // 데이터 값 분리
+            JObject jsonObject = new JObject();
+
+            for (int j = 0; j < headers.Length && j < values.Length; j++)
+            {
+                string cleanValue = values[j].Trim();
+                jsonObject[headers[j].Trim()] = cleanValue; // `+` 인코딩 안 함
+            }
+
+            jsonArray.Add(jsonObject);
+        }
+
+        return jsonArray.ToString();
     }
 }

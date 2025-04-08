d794e202d771fc9819134f29c9f3f019fa41ccf5
bnm03090805 <bnm03090805@gmail.com>
2025-04-08 14:38:34 +0900

    [feat] 현상금몬스터 작성 및 도트데미지 스턴 구현

---
+++ Assets/Team/KYJ/Script/BountyMonster.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BountyMonster : BaseMonster
+{
+    protected override void Attack()
+    {
+        Debug.Log("현상금몬스터공격");
+        attackTimer = attackDelay;
+    }
+
+    protected override void MonsterSkill()
+    {
+        Debug.Log($"현상금몬스터 {skillData.name} 사용");
+        skillTimer = skillData.skillCoolTime;
+    }
+}

2a1b10f40f7820e5ce461814688cb10fded031a4
rlck339 <a95895899@gmail.com>
2025-04-09 14:23:45 +0900

    Feat : 게임씬 UI 수정,마을가기추가

--- Assets/Team/LGW/000GW/GameUI/GameOptionUI.cs
+++ Assets/Team/LGW/000GW/GameUI/GameOptionUI.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class GameOptionUI : MonoBehaviour
 {
+    public GameObject optionSlot;
     public GameObject optionPanel;
-
+    public GameObject HomePanel;
     public void OpenOption()
     {
-        optionPanel.SetActive(true);
+        optionSlot.SetActive(!optionSlot.activeSelf);
     }
     public void CloseOption()
     {
+        optionSlot.SetActive(false);
+    }
+
+    public void CloseOptionPanel()
+    {
         optionPanel.SetActive(false);
     }
+    public void OpenOptionPanel()
+    {
+        optionPanel.SetActive(true);
+        optionSlot.SetActive(false);
+    }
+
+    public void CloseHomePanel()
+    {
+        HomePanel.SetActive(false);
+    }
+    public void OpenHomePanel()
+    {
+        HomePanel.SetActive(true);
+        optionSlot.SetActive(false);
+    }
+
 }

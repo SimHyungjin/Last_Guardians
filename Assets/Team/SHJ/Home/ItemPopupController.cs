72b598706bd07bcd2edb516ed0baaf6f6a84f261
SimHyungjin <tlagud26@naver.com>
2025-04-09 19:05:27 +0900

    wip : 머지용

--- Assets/Team/SHJ/Home/ItemPopupController.cs
+++ Assets/Team/SHJ/Home/ItemPopupController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemPopupController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject root;
+    [SerializeField] private Image icon;
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text description;
+
+    [SerializeField] private Button equipButton;
+    [SerializeField] private Button unequipButton;
+
+    private ItemData currentData;
+
+    public void Open(ItemData data)
     {
-        
+        currentData = data;
+        icon.sprite = data.icon;
+        itemName.text = data.itemName;
+        description.text = data.itemDescription;
+
+        if (data is EquipemntData equipData)
+        {
+            bool isEquipped = HomeManager.Instance.equipment.IsEquipped(equipData);
+            equipButton.gameObject.SetActive(!isEquipped);
+            unequipButton.gameObject.SetActive(isEquipped);
+        }
+        else
+        {
+            equipButton.gameObject.SetActive(false);
+            unequipButton.gameObject.SetActive(false);
+        }
+
+        root.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Close()
     {
-        
+        root.SetActive(false);
     }
+
+    //public void OnClickEquip()
+    //{
+    //    HomeManager.Instance.equipment.Equip(currentData);
+    //    Close();
+    //}
+
+    //public void OnClickUnEquip()
+    //{
+    //    Equipment.Instance.UnEquip(currentData);
+    //    Close();
+    //}
 }

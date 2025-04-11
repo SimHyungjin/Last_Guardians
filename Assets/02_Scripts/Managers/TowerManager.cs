04abb08ab4293393417536dbaecf3169208a3dc2
dongseok-kim <kdsehdtjr12@gmail.com>
2025-04-11 16:40:35 +0900

    Feat:InteractionState Fix:덱이랑 타워랑 동시에 클릭되는거 방지
    타워매니저에서 InteractionState생성해서
    FSM으로 동시클릭 방지

--- Assets/02_Scripts/Managers/TowerManager.cs
+++ Assets/02_Scripts/Managers/TowerManager.cs
@@ -2,9 +2,32 @@ using Unity;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
+public enum InteractionState
+{
+    None,
+    CardMoving,
+    TowerMoving,
+}
 public class TowerManager : Singleton<TowerManager>
 {
     public DeckHandler hand;
     public TowerCombinationData towerCombinationData;
     public Towerbuilder towerbuilder;
+
+    public InteractionState CurrentState { get; private set; } = InteractionState.None;
+    public bool CanStartInteraction()
+    {
+        return CurrentState == InteractionState.None;
+    }
+
+    public void StartInteraction(InteractionState newState)
+    {
+        CurrentState = newState;
+    }
+
+    public void EndInteraction(InteractionState endState)
+    {
+        if (CurrentState == endState)
+            CurrentState = InteractionState.None;
+    }
 }

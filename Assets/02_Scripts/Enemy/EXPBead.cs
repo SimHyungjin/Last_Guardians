350ab836418f533d39321acd1272996946a6798e
bnm03090805 <bnm03090805@gmail.com>
2025-04-22 09:30:44 +0900

    [feat] 경험치 구슬 시간지나면 사라지는 기능

--- Assets/02_Scripts/Enemy/EXPBead.cs
+++ Assets/02_Scripts/Enemy/EXPBead.cs
@@ -6,10 +6,24 @@ public class EXPBead : MonoBehaviour
 {
     private Collider2D collider;
     public int EXP { get; private set; }
+    private Coroutine disappearCorutine;
+    private float disTime = 30f;
+
 
     private void Awake()
     {
         collider = GetComponent<Collider2D>();
+    }
+
+    private void OnEnable()
+    {
+        disappearCorutine = StartCoroutine(Disappear());
+    }
+
+    private IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(disTime);
+        PoolManager.Instance.Despawn<EXPBead>(this);
     }
 
     public void Init(int exp, Transform monster)

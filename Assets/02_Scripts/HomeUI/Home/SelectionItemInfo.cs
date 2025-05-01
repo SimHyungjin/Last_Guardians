b2adad2d7f9eb7a52fdc83e46d921666484072bc
SimHyungjin <tlagud26@naver.com>
2025-05-01 12:16:28 +0900

    chore : 이미지 재 삽입

---
+++ Assets/02_Scripts/HomeUI/Home/SelectionItemInfo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SelectionItemInfo : MonoBehaviour
+{
+    [SerializeField] SelectionController selectionController;
+
+    [SerializeField] private TextMeshProUGUI attackTypeText;
+    [SerializeField] private TextMeshProUGUI attackPowerText;
+    [SerializeField] private TextMeshProUGUI attackSpeedText;
+    [SerializeField] private TextMeshProUGUI attackRangeText;
+    [SerializeField] private TextMeshProUGUI moveSpeedText;
+    [SerializeField] private TextMeshProUGUI criticalChanceText;
+    [SerializeField] private TextMeshProUGUI criticalDamageText;
+    [SerializeField] private TextMeshProUGUI penetrationText;
+
+}

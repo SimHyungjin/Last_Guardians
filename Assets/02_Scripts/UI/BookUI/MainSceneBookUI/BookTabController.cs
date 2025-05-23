b1d3b61ee4a19eb1611d69272724a5fcca5266ec
rlck339 <a95895899@gmail.com>
2025-05-23 10:30:23 +0900

    Fix : 빠진거수정

--- Assets/02_Scripts/UI/BookUI/MainSceneBookUI/BookTabController.cs
+++ Assets/02_Scripts/UI/BookUI/MainSceneBookUI/BookTabController.cs
@@ -1,50 +1,24 @@
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BookTabController : MonoBehaviour
 {
-    [Header("Panels")]
     public GameObject towerBookPanel;
     public GameObject towerComboPanel;
-
-    [Header("Tab Buttons")]
-    [SerializeField] private Button bookTabButton;
-    [SerializeField] private Button comboTabButton;
-
-    [Header("Tab Colors")]
-    [SerializeField] private Color defaultColor = Color.white;
-    [SerializeField] private Color selectedColor = Color.yellow;
 
     private void Start()
     {
         ShowTowerBook();
     }
-
     public void ShowTowerBook()
     {
-     
         towerBookPanel.SetActive(true);
         towerComboPanel.SetActive(false);
-
-     
-        HighlightTab(bookTabButton);
     }
-
     public void ShowTowerCombo()
     {
         towerBookPanel.SetActive(false);
         towerComboPanel.SetActive(true);
-
-        HighlightTab(comboTabButton);
-    }
-
-    private void HighlightTab(Button selected)
-    {
-      
-        bookTabButton.image.color = defaultColor;
-        comboTabButton.image.color = defaultColor;
-
-       
-        selected.image.color = selectedColor;
     }
 }
